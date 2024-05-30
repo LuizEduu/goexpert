fe6f606eda58f0fa612b07442d5ac22c538f76f9
Luiz <l.eduardo8316@gmail.com>
2024-05-30 13:37:08 -0300

    finally basic

---
+++ basicLanguage/21/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	/* for i := 0; i < 10; i++ {
+		println(i)
+	} */
+
+	/* numbers := []string{"um", "dois", "tres", "quatro", "cinco"}
+
+	for _, v := range numbers {
+		println(v)
+	} */
+
+}
